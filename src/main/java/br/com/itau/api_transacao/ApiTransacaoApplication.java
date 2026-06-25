83b3fd71a5515b8cce41f56df21a5c0eca24ae0f
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-25 18:32:05 -0300

    Commit inicial do projeto.

---
+++ src/main/java/br/com/itau/api_transacao/ApiTransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.api_transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itau/api_transacao/ApiTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.api_transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
